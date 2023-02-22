d362d11e80c92614141fecbe2aa0f7e1b61c1d58
amandacolacioRethink <amanda.colacio@rethink.dev>
2023-02-22 16:14:02 -0300

    Desafio - aula 5 (atualização com clean code)

--- desafio/challenge.js
+++ desafio/challenge.js
@@ -11,19 +11,17 @@ const lookForPokemonByName = (name) => {
   }
 };
 
-//Listar todos os pokemons de um tipo
-const listAllPokemonsOfAType = (type) => {
+const listAllPokemonsByType = (type) => {
   var list = [];
   for (let i = 0; i < pokemons.length; i++) {
     if (pokemons[i].type[0] == type || pokemons[i].type[1] == type) {
-      list.push(pokemons[i]); // criar um array pra armazenar todos os pokemons dentro
+      list.push(pokemons[i]);
     }
   }
   console.log(list);
 };
 
-//Alterar o nome dos tipos dos pokemons para ser em português, ex.: Fire => Fogo
-const changeNameOfPokemonsTypesToPortuguese = () => {
+const changePokemonTypeNamesToPortuguese = () => {
   for (let i = 0; i < pokemons.length; i++) {
     for (let j = 0; j < pokemons[i].type.length; j++) {
       pokemons[i].type[j] = translatePokemonTypeNames[pokemons[i].type[j]];
