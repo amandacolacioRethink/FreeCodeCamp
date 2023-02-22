394f8b05666864dbf74b77193b91bc1efd462809
amandacolacioRethink <amanda.colacio@rethink.dev>
2023-02-22 11:51:41 -0300

    Desafio

--- desafio/desafio.js
+++ desafio/desafio.js
@@ -1,12 +1,25 @@
 import { pokemons } from "./data.js";
 
-function ProcurarPokemon(nome) {
+//Procurar um pokemon por nome
+const procurarPokemon = (nome) => {
   for (let i = 0; i < pokemons.length; i++) {
     if (pokemons[i].name == nome) {
-      var poke = pokemons[i];
-      console.log(poke.type[1]);
+      return pokemons[i];
+      //console.log(pokemonNome);
     }
   }
-}
+};
 
-ProcurarPokemon("bulbasaur");
+//Listar todos os pokemons de um tipo
+const listarPokemon = (tipo) => {
+  for (let i = 0; i < pokemons.length; i++) {
+    if (pokemons[i].type[0] == tipo || pokemons[i].type[1] == tipo) {
+      var lista = [];
+      lista.push(pokemons[i]); // criar um array pra armazenar todos os pokemons dentro
+      //console.log(lista);
+      return lista;
+    }
+  }
+};
+
+//Alterar o nome dos tipos dos pokemons para ser em português, ex.: Fire => Fogo
